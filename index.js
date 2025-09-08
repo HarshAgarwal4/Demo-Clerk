6f2e9b752f36c0e92632d0792537e990d49dc323
HarshAgarwal4 <agarwalh270@gmail.com>
2025-09-08 20:58:39 +0530

    inkitial

---
+++ index.js
@@ -0,0 +1,31 @@
+import 'dotenv/config'
+import express from 'express'
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const PORT = 3000
+
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
+app.use(cookieParser())
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.get('/' , (req,res)=> {
+    console.log("Hello")
+    res.send("Hello world")
+})
+
+app.get('/protected' , requireAuth , (req,res)=>{
+    res.send({status:1})
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
